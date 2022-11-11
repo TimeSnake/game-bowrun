0bb9cbcb7b7dda0cb781994d97da7491a640ea0d
funzter <cavehive@protonmail.com>
2022-11-11 20:31:16 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/game/bowrun/chat/Plugin.java
+++ src/main/java/de/timesnake/game/bowrun/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * game-bowrun.main
+ * timesnake.game-bowrun.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,11 +18,16 @@
 
 package de.timesnake.game.bowrun.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.loungebridge.util.chat.Plugin {
 
-    public static final Plugin BOWRUN = new Plugin("BowRun", "BWR");
+    public static final Plugin BOWRUN = new Plugin("BowRun", "BWR", LogHelper.getLogger("BowRun", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
